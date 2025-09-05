ca6e2f7ceac929583429b9bc07ebe95ccb785c57
snowari <70631889+snowari@users.noreply.github.com>
2025-09-06 01:08:09 +0900

    ✨ 로그인, 최초정보등록 페이지 퍼블리싱

---
+++ client/src/components/auth/LoginBanner.tsx
@@ -0,0 +1,19 @@
+const LoginBanner = () => {
+  return (
+    <div className="flex flex-col w-full  ">
+      <h2 className="font-black text-[var(--color-black)] text-lg py-8">
+        NUNEW
+      </h2>
+      <h1 className=" text-[var(--color-black)] leading-[140%] text-4xl font-bold">
+        관심있는 뉴스만
+        <br />
+        내맘대로.
+      </h1>
+      <h3 className="text-[var(--color-gray-80)] text-sm pt-2.5">
+        서브 슬로건을 정해볼까요?
+      </h3>
+    </div>
+  );
+};
+
+export default LoginBanner;

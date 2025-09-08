5e1ff6ac134a4f111a9a7aca66fecb625be3ec0e
Bomin Kim <sirubom1203@gmail.com>
2025-09-08 22:47:52 +0900

    [style] 쇼츠 스크랩버튼 추가 및 세부 수정

--- client/src/components/home/NewsSection.tsx
+++ client/src/components/home/NewsSection.tsx
@@ -1,8 +1,9 @@
 import Image, { StaticImageData } from "next/image";
 import { TextButton } from "../ui/TextButton";
 import { Eye, ThumbsUp } from "lucide-react";
-import { IoBookmarkSharp } from "react-icons/io5";
+import { IoBookmarkOutline, IoBookmark } from "react-icons/io5";
 import { IconButton } from "../ui/IconButton";
+import { useState } from "react";
 
 interface NewsData {
   id: number;
@@ -19,12 +20,18 @@ export default function NewsSection({
   className: string;
   data: NewsData;
 }) {
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  const handleBookmark = () => {
+    setIsBookmarked(!isBookmarked);
+  };
+
   return (
     <>
       <section
         className={`relative w-full min-h-[100dvh] bg-[url('/images/handsomeLee.png')] bg-no-repeat bg-cover bg-center ${className}`}
       >
-        <div className="absolute w-full inset-0 bg-black/70 backdrop-blur-[28px] z-0" />
+        <div className="absolute w-full inset-0 bg-[var(--color-black)]/70 backdrop-blur-[28px] z-0" />
         <div className="relative w-full z-10 px-5 flex flex-col">
           <div className="pt-[113px] max-h-screen">
             <div className="flex w-full min-w-80 h-90 [@media(max-height:700px)]:h-60  mx-auto justify-center overflow-hidden">
@@ -37,8 +44,8 @@ export default function NewsSection({
                 className="object-cover w-full min-w-80 rounded-2xl"
               />
             </div>
-            <div className="flex mt-8 cursor-default [@media(max-height:700px)]:mt-4 w-full justify-between">
-              <div className="mr-6">
+            <div className="flex mt-7 cursor-default [@media(max-height:700px)]:mt-4 w-full justify-between">
+              <div className="mr-6 flex-1">
                 <div className="flex gap-0.5">
                   <Image
                     src={data.categoryIcon}
@@ -49,14 +56,14 @@ export default function NewsSection({
                   />
                   <p className="text-[var(--color-white)]">{data.category}</p>
                 </div>
-                <div className="flex flex-col cursor-default gap-2 mt-2">
-                  <h1 className="text-lg font-bold leading-[140%] text-[var(--color-white)]">
+                <div className="flex flex-col cursor-default gap-2 mt-2 [@media(max-height:700px)]:gap-0.5">
+                  <h1 className="text-lg font-bold text-[var(--color-white)] line-clamp-2">
                     {data.title}
                   </h1>
-                  <span className="text-[var(--color-gray-60)] text-sm mt-2 text-ellipsis line-clamp-3 [@media(max-height:700px)]:line-clamp-2">
+                  <span className="text-[var(--color-gray-60)] text-sm mt-2 text-ellipsis line-clamp-3 [@media(max-height:70px)]:line-clamp-2">
                     {data.description}
                   </span>
-                  <div className="flex gap-[7px] mt-20 [@media(max-height:700px)]:mt-5">
+                  <div className="flex gap-[7px] mt-14 [@media(max-height:700px)]:mt-4">
                     <TextButton className="w-[97px] h-9 px-4 bg-[var(--color-white)]/10 hover:bg-[var(--color-white)]/15">
                       <p className="text-sm whitespace-nowrap text-transparent bg-clip-text bg-gradient-to-r from-[#F0FFBC] to-[var(--color-primary-40)]">
                         AI 세줄요약
@@ -68,19 +75,31 @@ export default function NewsSection({
                   </div>
                 </div>
               </div>
-              <div className="flex flex-col justify-between">
-                <div className="cursor-pointer">
-                  <IoBookmarkSharp className="text-[var(--color-white)] w-7 h-7" />
-                </div>
-                <div className="flex flex-col gap-3">
+              <div className="flex flex-col justify-end">
+                <div className="flex flex-col [@media(max-height:700px)]:gap-4 gap-6">
                   <div className="flex flex-col gap-1.5">
-                    <p className="text-[var(--color-white)] text-[13px]  font-normal">
+                    <IconButton
+                      icon={isBookmarked ? IoBookmark : IoBookmarkOutline}
+                      className={`cursor-pointer transition-opacity duration-300 ${
+                        isBookmarked ? "opacity-100" : "opacity-80"
+                      }`}
+                      color="var(--color-white)"
+                      size={24}
+                      onClick={handleBookmark}
+                    />
+                    <p className="text-[var(--color-white)] text-xs font-normal whitespace-nowrap">
+                      스크랩
+                    </p>
+                  </div>
+                  <div className="flex flex-col gap-1.5 items-center">
+                    <ThumbsUp className="text-[var(--color-white)] text-center" />
+                    <p className="text-[var(--color-white)] text-[13px] font-normal text-center">
                       255
                     </p>
                   </div>
-                  <div className="flex flex-col gap-1">
+                  <div className="flex flex-col gap-1 items-center">
                     <Eye className="text-[var(--color-white)]" />
-                    <p className="text-[var(--color-white)] text-[13px] font-normal">
+                    <p className="text-[var(--color-white)] text-[13px] font-normal text-center">
                       255
                     </p>
                   </div>

16c4739c5afb330e4ef96d9bbcb80e975776f25f
snowari <70631889+snowari@users.noreply.github.com>
2025-09-09 19:25:45 +0900

    💄 Style 다크모드 관련 설정

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,12 +2,12 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 export const metadata: Metadata = {
   title: "NUNEW",
   description: "누구나 간단히 읽는 쉬운 뉴스, 누뉴",
 };
-
 const pretendard = localFont({
   src: "../../public/fonts/PretendardVariable.woff2",
   display: "swap",
@@ -23,8 +23,15 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${pretendard.variable} w-full`}>
-        <div className="max-w-screen-lg mx-auto">{children}</div>
-        <Toaster position="top-center" />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem={false} // 시스템 설정 무시
+          disableTransitionOnChange
+        >
+          <div className="max-w-screen-lg mx-auto">{children}</div>
+          <Toaster position="top-center" />
+        </ThemeProvider>
       </body>
     </html>
   );

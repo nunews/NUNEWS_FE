a490f7374e19b924286075c8ea48c0ef748bd48e
kang-min-ryu <dbrkdalswkd@gmail.com>
2025-11-06 16:57:13 +0900

    ✨ Feat: 랜덤 닉네임 기능 추가

---
+++ client/src/types/profile.d.ts
@@ -0,0 +1,9 @@
+export interface ProfileEditFormProps {
+  nickname: string;
+  setNickname: (value: string) => void;
+  currentNickname: string;
+  currentProfileImage: string | null;
+  setProfileImage?: (value: string) => void;
+  setIsNicknameChecked: (value: boolean) => void;
+  isNicknameChecked: boolean;
+}

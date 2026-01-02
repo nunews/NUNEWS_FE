b60b029449184f57c0462e5fb3fb7d0504269538
Bomin Kim <sirubom1203@gmail.com>
2026-01-02 15:15:49 +0900

    refactor: 상수 분리

---
+++ client/src/lib/constants/ageOptions.ts
@@ -0,0 +1,7 @@
+export const ageOptions = [
+  { label: "10대", value: "10s" },
+  { label: "20대", value: "20s" },
+  { label: "30대", value: "30s" },
+  { label: "40대 이상", value: "40s" },
+  { label: "선택하지 않음", value: "none" },
+];

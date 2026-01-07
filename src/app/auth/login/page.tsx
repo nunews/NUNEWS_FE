e224f6831d9e9cadb478218d1a779c82f03eb442
Bomin Kim <sirubom1203@gmail.com>
2026-01-07 23:59:15 +0900

    refactor: comment props 수정

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -14,8 +14,8 @@ export default function LoginPage() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${location.origin}/auth/callback`,
-        //redirectTo: `http://localhost:3000/auth/callback`,
+        // redirectTo: `${location.origin}/auth/callback`,
+        redirectTo: `http://localhost:3000/auth/callback`,
       },
     });
 
